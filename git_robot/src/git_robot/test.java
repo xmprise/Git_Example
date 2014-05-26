3ad6786367dece54b0545df4798b434ef83f01f4
LG <LG@LG-PC>
2014-05-26 14:09:59 +0900

    sasdasd

--- git_robot/src/git_robot/test.java
+++ git_robot/src/git_robot/test.java
@@ -1,7 +1,7 @@
 package git_robot;
 
 public class test {
-public static void main(String[] args) {
-	System.out.println("hello");
-}
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
 }
